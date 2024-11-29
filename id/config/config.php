1f0dd7c057596eee1020141e665fcb4c47fd4387
Reza Saputra <129853013+rezasaputra4250@users.noreply.github.com>
2024-11-29 18:54:19 +0700

    Mengubah file: id/config/config.php

--- id/config/config.php
+++ id/config/config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');  // Ganti dengan username MySQL Anda
 define('DB_PASSWORD', '');      // Ganti dengan password MySQL Anda
-define('DB_NAME', 'accounting_system');  // Ganti dengan nama database Anda
+define('DB_NAME', 'accounting');  // Ganti dengan nama database Anda
 
 // Koneksi ke database
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);

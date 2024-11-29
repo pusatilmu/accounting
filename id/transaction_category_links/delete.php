40bb150eb8a6debf1c7c927753a9bd363fee5867
Reza Saputra <129853013+rezasaputra4250@users.noreply.github.com>
2024-11-29 14:52:20 +0700

    Menambahkan file baru: id/transaction_category_links/delete.php

---
+++ id/transaction_category_links/delete.php
@@ -0,0 +1,19 @@
+<?php
+include_once '../config/db.php';
+
+if (isset($_GET['id']) && isset($_GET['category_id'])) {
+  $transaction_id = $_GET['id'];
+  $category_id = $_GET['category_id'];
+
+  // SQL untuk menghapus link berdasarkan transaction_id dan category_id
+  $sql = "DELETE FROM transaction_category_links WHERE transaction_id = $transaction_id AND category_id = $category_id";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "Tautan berhasil dihapus.";
+    header("Location: index.php");
+  } else {
+    echo "Kesalahan: " . $conn->error;
+  }
+
+  $conn->close();
+}

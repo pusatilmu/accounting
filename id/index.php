19872d3f9fbebbe03868205b4a16682799893dcc
Reza Saputra <129853013+rezasaputra4250@users.noreply.github.com>
2024-11-29 14:52:19 +0700

    Menambahkan file baru: id/index.php

---
+++ id/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Beranda</title>
+</head>
+
+<body>
+  <h1>Selamat Datang di Aplikasi Akuntansi</h1>
+  <ul>
+    <li><a href="accounts/index.php">Manajemen Akun</a></li>
+    <li><a href="transaction_categories/index.php">Kategori Transaksi</a></li>
+    <li><a href="transaction_category_links/index.php">Tautan Kategori Transaksi</a></li>
+    <li><a href="transaction_details/index.php">Detail Transaksi</a></li>
+    <li><a href="transactions/index.php">Transaksi</a></li>
+  </ul>
+</body>
+
+</html>

cd91dbe2b3001c089d9c3c5f9d4d0e61a28ba3a3
Reza Saputra <129853013+rezasaputra4250@users.noreply.github.com>
2024-11-29 14:52:15 +0700

    Menambahkan file baru: en/index.php

---
+++ en/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+</head>
+
+<body>
+  <h1>Home</h1>
+  <ul>
+    <li><a href="accounts/index.php">Accounts</a></li>
+    <li><a href="transaction_categories/index.php">Transaction Categories</a></li>
+    <li><a href="transaction_category_links/index.php">Transaction Category Links</a></li>
+    <li><a href="transaction_details/index.php">Transaction Details</a></li>
+    <li><a href="transactions/index.php">Transactions</a></li>
+  </ul>
+</body>
+
+</html>
